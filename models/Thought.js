d5fef2088cd20452d19342f10aa03e9d5a78984f
Angeleena <angeleenasyse@gmail.com>
2021-07-22 16:12:29 -0700

    thought model created, and date helper file added

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,24 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: []
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
